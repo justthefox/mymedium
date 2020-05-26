9816d42a2e3a2289a504d96906add069b8fa8a11
Lyubov <spiridonova73l@gmail.com>
2020-05-26 22:08:00 +0400

    build

---
+++ docs/precache-manifest.a6c2b5e61c81dd8fbb75d97ff991fe03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4535cc6ce64ac9af4af2c5a252273843",
+    "url": "./index.html"
+  },
+  {
+    "revision": "860505bfb2e8408f82dd",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "860505bfb2e8408f82dd",
+    "url": "./static/js/2.45069011.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.45069011.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06a487cfd0363e7df967",
+    "url": "./static/js/main.4f858f40.chunk.js"
+  },
+  {
+    "revision": "1e673d583b25847b56fa",
+    "url": "./static/js/runtime-main.d1213faf.js"
+  }
+]);
